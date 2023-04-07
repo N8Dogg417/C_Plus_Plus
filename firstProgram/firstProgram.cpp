ae5f77151945445bcff7f3a78955bf6e5efd4f7e
N8Dogg417 <Nathan.H.Desso@gmail.com>
2023-04-06 17:56:54 -0700

    Add project files.

---
+++ firstProgram/firstProgram.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "My name is Nadilina"<< endl;
+    std::cout << "Today is a good day....";
+    std::cout << "testing, testing.";
+    system("pause>0");
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
